111ea7daf5e3afdd6f979a458a85eeed493423ec
HWein8 <hadariweinstein@gmail.com>
2023-05-03 09:37:57 -0400

    prisma configured

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 const express = require('express');
-const pgController = require('./controller/pgController');
+// const pgController = require('./controller/pgController');
+const testController = require('./controller/testController');
 const path = require('path');
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const cors = require('cors');
+const app = express();
+app.use(cors());
 const PORT = 3000;
 
 app.use(express.json());
@@ -16,14 +17,29 @@ app.use('/localhost:3000', express.static(path.join(__dirname, '../public')));
 // 	return res.status(200).json({ message: 'hello' });
 // });
 
-app.get('/', pgController.getTrain, (req, res) => {
-	return res.status(200).send(res.locals.list);
-});
-app.post('/post', pgController.postMessage, pgController.getTrain, (req, res) => {
-
+// app.get('/', pgController.getTrain, (req, res) => {
+// 	return res.status(200).send(res.locals.list);
+// });
+// app.post('/post', pgController.postMessage, pgController.getTrain, (req, res) => {
 	// console.log("RESPONSE: ", res)
 	// console.log(res.locals.list[ 0 ])
-	return res.status(200).json(res.locals.list);
+// 	return res.status(200).json(res.locals.list);
+// });
+
+app.get('/test', testController.createUser, (req, res) => {
+	return res.sendStatus(200);
+});
+
+app.get('/getUsers', testController.findUsers, (req, res) => {
+	return res.status(200).json(res.locals.users);
+});
+
+app.get('/getTrains', testController.findTrains, (req, res) => {
+	return res.status(200).json(res.locals.trains);
+});
+
+app.get('/getComplaints', testController.findComplaints, (req, res) => {
+	return res.status(200).json(res.locals.complaints);
 });
 
 // app.delete('/list/:id', pgController.deleteListItem, (req, res) => {
