3f29fae8e38db5496fdd30d6af9f639850ed2397
Yohan Yang <yangyohan1@gmail.com>
2023-05-03 16:52:15 -0400

    started endpoint testing

---
+++ __test__/supertest.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+
+const server = 'http://localhost:3000';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Route integration', () => {
+  describe('/', () => {
+  describe('GET', () => {
+        // Note that we return the evaluation of `request` here! It evaluates to
+        // a promise, so Jest knows not to say this test passes until that
+        // promise resolves. See https://jestjs.io/docs/en/asynchronous
+    it('responds with 200 status and text/html content type', () => {
+          return request(server)
+            .get('/')
+            .expect('Content-Type', /text\/html/)
+            .expect(200);
+        });
+      });
+    });
+});
