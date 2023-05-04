836fa1c85002ebd3802711f55d6263bb340a888f
Ariel Lin <al2248@cornell.edu>
2023-05-04 12:07:24 -0400

    added header styling and made subway visual larger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,24 @@ import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 
-export default function Navbar () {
+export default function Navbar() {
   const navigate = useNavigate();
-  const currentDisplay = useSelector( (state) => state.account.isLoggedIn)
+  const currentDisplay = useSelector((state) => state.account.isLoggedIn)
   return (
     <div>
-      <ul>
+      <ul className='navBarCSS'>
         <li className="list-options">
-        <Link className="home" to="/">Home</Link>
-        </li> 
-          {(!currentDisplay) ? [<li className="list-options">
-            <Link className="login" to="/login">Login</Link>
-          </li>,
-          <li className="list-options">
-            <Link className="signup" to="/signup">Signup</Link>
-          </li>] : null
-    
-          }
+          <Link className="home" to="/">Home</Link>
+        </li>
+        {(!currentDisplay) ? [<li className="list-options">
+          <Link className="" to="/login">Login</Link>
+        </li>,
+        <li className="list-options">
+          <Link className="signup" to="/signup">Signup</Link>
+        </li>] : null
+        }
       </ul>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
